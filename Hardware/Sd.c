265531d028f0997dbbe38fb12997124cfdb1ab5d
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-07 23:47:19 -0300

    Update Sd.c

--- Hardware/Sd.c
+++ Hardware/Sd.c
@@ -1,11 +1,11 @@
 extern "SdCard" : {
 // filing cabinet operation
-fopen("%a:in folder");
-fread("%n:to folder");
-fopen("%a:in file");
-fread("%n:to file");
-break("unfolder");
-break("unfile");
+fopen("%a:infolder");
+fread("%n:folder data");
+fopen("%a:infile");
+fread("%n:file data");
+break("undir");
+break("undoc");
 fseek("\?:unknown");
 ftell("\a:access to sd device");
 fwrite("\n:ask for file write permission");
