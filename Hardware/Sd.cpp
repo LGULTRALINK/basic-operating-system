df4b8937516d045f80ecde20ed6c09ae547f9d83
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-07 23:59:28 -0300

    Update Sd.cpp

--- Hardware/Sd.cpp
+++ Hardware/Sd.cpp
@@ -1,9 +1,5 @@
 extern "SdCard" : {
 // filing cabinet operation
-fopen("%a:in-folder");
-fread("%n:folder data");
-fopen("%a:in-file");
-fread("%n:file data");
 fseek("\?:unknown");
 ftell("\a:access to sd device");
 fscan("\\:accessing root of sd device");
@@ -13,5 +9,8 @@ remove("afolder");
 remove("afile");
 fwrite("\n:ask for file write permission");
 sizeof("%%:amount of space occupied");
-fclose(exit);
+fopen("entry of folder");
+fopen("entry of file");
+fclose("exit of folder");
+fclose("exit of file");
 };
