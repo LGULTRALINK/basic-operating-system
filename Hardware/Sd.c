05b95e0e4dd82d7a427872cea63e4e6aa1c548a5
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-07 23:45:20 -0300

    Create Sd.c

---
+++ Hardware/Sd.c
@@ -0,0 +1,14 @@
+extern "SdCard" : {
+// filing cabinet operation
+fopen("%a:in folder");
+fread("%n:to folder");
+fopen("%a:in file");
+fread("%n:to file");
+break("unfolder");
+break("unfile");
+fseek("\?:unknown");
+ftell("\a:access to sd device");
+fwrite("\n:ask for file write permission");
+sizeof("%%:amount of space occupied");
+fclose(exit);
+};
