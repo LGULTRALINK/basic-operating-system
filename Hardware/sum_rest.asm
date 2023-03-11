48aafa1e160863af09d2a117ca8c920d6922565b
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-11 13:04:37 -0300

    Update sum_rest.asm

--- Hardware/sum_rest.asm
+++ Hardware/sum_rest.asm
@@ -1,4 +1,4 @@
-	IN/OUT == val[$]; sum of money
+	IN/OUT == val max[+$0]; sum of money
 	add	[0 > 1 = $10]
 	add	[0 > 2 = $20]
 	add	[0 > 3 = $30]
@@ -9,7 +9,7 @@
 	add	[0 > 8 = $80]
 	add	[0 > 9 = $90]
 	add	[0 > 10 = $100]
-	OUT/IN val[$]; rest of money
+	OUT/IN val min[-$0]; rest of money
 	sub	[0 < 1 = +1]
 	sub	[0 < 2 = +2]
 	sub	[0 < 3 = +3]
