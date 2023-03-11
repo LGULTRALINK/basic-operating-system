118acd3cb01462345a828960772d2db4d9c23aa1
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-10 22:33:07 -0300

    Update sysboot.asm

--- Hardware/sysboot.asm
+++ Hardware/sysboot.asm
@@ -1,4 +1,4 @@
-	!	MOVSW	:	(ALU); arithmetic logic unit
+	WORD	:	(arithmetic logic unit)
 	.	ADD	[0 > 1 = +10]	,	SUB	[2 < 1 = -1]
 	.	ADD	[0 > 2 = +20]	,	SUB	[2 < 2 = ±0]
 	.	ADD	[0 > 3 = +30]	,	SUB	[2 < 3 = +1]
@@ -18,7 +18,7 @@
 	.	MUL	[2 × 8 = *16]	,	DIV	[2 ÷ 8 = #4]
 	.	MUL	[2 × 9 = *18]	,	DIV	[2 ÷ 9 = #4.5]
 	.	MUL	[2 × 10 = *20]	,	DIV	[2 ÷ 10 = #5]
-	!	LODSB	:	(BIOS); Basic Input/Output System
+	WORD 	:	(Basic Input/Output System)
 	∧	AND	[OUT/OUT : (Kb) = 103]	∨	OR	[IN/IN : (Kib) = 210];
 	∧	AND	[OUT/OUT : (Mb) = 106]	∨	OR	[IN/IN : (Mib) = 220];
 	∧	AND	[OUT/OUT : (Gb) = 109]	∨	OR	[IN/IN : (Gib) = 230];
@@ -27,32 +27,34 @@
 	∧	AND	[OUT/OUT : (Eb) = 1018]	∨	OR	[IN/IN : (Eib) = 260];
 	∧	AND	[OUT/OUT : (Zb) = 1021]	∨	OR	[IN/IN : (Zib) = 270];
 	∧	AND	[OUT/OUT : (Yb) = 1024]	∨	OR	[IN/IN : (Yib) = 280];
-	¬	NOT	[IN/OUT : (kB) = 103]	⌐	IF	[OUT/IN : (KiB) = 210];
-	¬	NOT	[IN/OUT : (MB) = 106]	⌐	IF	[OUT/IN : (MiB) = 220];
-	¬	NOT	[IN/OUT : (GB) = 109]	⌐	IF	[OUT/IN : (GiB) = 230];
-	¬	NOT	[IN/OUT : (TB) = 1012]	⌐	IF	[OUT/IN : (TiB) = 240];
-	¬	NOT	[IN/OUT : (PB) = 1015]	⌐	IF	[OUT/IN : (PiB) = 250];
-	¬	NOT	[IN/OUT : (EB) = 1018]	⌐	IF	[OUT/IN : (EiB) = 260];
-	¬	NOT	[IN/OUT : (ZB) = 1021]	⌐	IF	[OUT/IN : (ZiB) = 270];
-	¬	NOT	[IN/OUT : (YB) = 1024]	⌐	IF	[OUT/IN : (YiB) = 280];
-	!	LODSW	:	(CPU); Central Processing Unit
-	PUSH	[AX],	POP	[BX]; accumulator/register
-	PUSH	[AH],	POP	[AL]; upper/lower
-	PUSH	[BP],	POP	[SP]; base index/stack index
-	PUSH	[CX],	POP	[DX]; counter/data
-	PUSH	[CS],	POP	[DS]; code segment/data segment
-	PUSH	[SS],	POP	[ES]; extra data segment/stack segment
-	PUSH	[SI],	POP	[DI]; source index/destination index
-	PUSH	[CH],	POP	[IP]; source address/program index
-	PUSH	[DH],	POP	[DL]; overflow/interrupt indicator
-	!	MOVSB	:(ROM); ReadOnly Memory
-	MOV	(9 % 1 = 512)
-	MOV	(9 % 2 = 1024)
-	MOV	(9 % 3 = 1536)
-	MOV	(9 % 4 = 2048)
-	MOV	(9 % 5 = 2560)
-	MOV	(9 % 6 = 3072)
-	MOV	(9 % 7 = 3584)
-	MOV	(9 % 8 = 4096)
-	MOV	(9 % 9 = 4608)
-	MOV	(9 % 10 = 5120)
+	¬	NOT	[IN/OUT : (kB) = 103]	⌐	NEG	[OUT/IN : (KiB) = 210];
+	¬	NOT	[IN/OUT : (MB) = 106]	⌐	NEG	[OUT/IN : (MiB) = 220];
+	¬	NOT	[IN/OUT : (GB) = 109]	⌐	NEG	[OUT/IN : (GiB) = 230];
+	¬	NOT	[IN/OUT : (TB) = 1012]	⌐	NEG	[OUT/IN : (TiB) = 240];
+	¬	NOT	[IN/OUT : (PB) = 1015]	⌐	NEG	[OUT/IN : (PiB) = 250];
+	¬	NOT	[IN/OUT : (EB) = 1018]	⌐	NEG	[OUT/IN : (EiB) = 260];
+	¬	NOT	[IN/OUT : (ZB) = 1021]	⌐	NEG	[OUT/IN : (ZiB) = 270];
+	¬	NOT	[IN/OUT : (YB) = 1024]	⌐	NEG	[OUT/IN : (YiB) = 280];
+	WORD	:	(Central Processing Unit)
+	TEST	[AX],	ADC	[BX]; accumulator/register
+	TEST	[AH],	ADC	[AL]; upper/lower
+	TEST	[BP],	ADC	[SP]; base index/stack index
+	TEST	[CX],	ADC	[DX]; counter/data
+	TEST	[CS],	ADC	[DS]; code segment/data segment
+	TEST	[SS],	ADC	[ES]; extra data segment/stack segment
+	TEST	[SI],	ADC	[DI]; source index/destination index
+	TEST	[CH],	ADC	[IP]; source address/program index
+	TEST	[DH],	ADC	[DL]; data head/data load
+	WORD	:(Read Only Memory)
+	MOV	[9 % 1 = 512]
+	MOV	[9 % 2 = 1024]
+	MOV	[9 % 3 = 1536]
+	MOV	[9 % 4 = 2048]
+	MOV	[9 % 5 = 2560]
+	MOV	[9 % 6 = 3072]
+	MOV	[9 % 7 = 3584]
+	MOV	[9 % 8 = 4096]
+	MOV	[9 % 9 = 4608]
+	MOV	[9 % 10 = 5120]
+	WORD	(Ramdom Access Memory)
+	
