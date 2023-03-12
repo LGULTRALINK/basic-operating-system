d81e7f288909c4369b343937258a353258441054
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-11 23:01:13 -0300

    Update sysboot.asm

--- Hardware/sysboot.asm
+++ Hardware/sysboot.asm
@@ -1,13 +1,1 @@
-	MOV	AX,PUSH	[AH]	POP	[AL] ;	accumulator
-	MOV	BX,PUSH	[BH]	POP	[BL] ;	base record
-	MOV	CX, ;	counter
-	MOV	DX, ;	data
-	MOV	CS, ;	code segment
-	MOV	DS, ;	data segment
-	MOV	ES, ;	extra segment
-	MOV	SS, ;	stack segment
-	MOV	IP, ;	program index
-	MOV	SI, ;	origin index
-	MOV	DI, ;	address index
-	MOV	SP, ;	stack index
-	MOV	BP, ;	base index
+
