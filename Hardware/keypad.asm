755b39b2ff67fbebde286d95d87d7b1900bcf8a1
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-14 00:07:07 -0300

    Update keypad.asm

--- Hardware/keypad.asm
+++ Hardware/keypad.asm
@@ -1,3 +1,11 @@
-	CALL "ON/OFF"; Power-Up Display / Power-Down Display
-LEA "ASCII"
-RET 
+	CALL ["ON/OFF"]; Power-Up Display / Power-Down Display
+LEA 00H PUSH '␀'; null state
+LEA 01H PUSH '␁'; start from header
+LEA 02H PUSH '␂'; begin writing text
+LEA 03H PUSH '␃'; finish text writing
+LEA 04H PUSH '␄'; end of transmission
+LEA 05H PUSH '␅';
+LEA 06H PUSH '␆';
+LEA 07H PUSH '␇';
+LEA 08H PUSH '␈';
+LEA 09H PUSH '␉';
