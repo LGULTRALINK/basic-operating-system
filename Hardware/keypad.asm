d94dfdfc6e4477869a555c83f6bc5c3eff234681
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-13 23:54:14 -0300

    Update keypad.asm

--- Hardware/keypad.asm
+++ Hardware/keypad.asm
@@ -1,28 +1,3 @@
 	CALL "ON/OFF"; Power-Up Display / Power-Down Display
-	CALL "ON/OFF"; Power-Up Display / Power-Down Display
-	PUSH !
-	PUSH " 
-	PUSH # 
-	PUSH $ 
-	PUSH % 
-	PUSH & 
-	PUSH ' 
-	PUSH ( 
-	PUSH ) 
-	PUSH * 
-	PUSH 
-	PUSH + 
-	PUSH , 
-	PUSH - 
-	PUSH . 
-	PUSH / 
-	PUSH 0 
-	PUSH 1 
-	PUSH 2 
-	PUSH 3 
-	PUSH 4 
-	PUSH 5 
-	PUSH 6 
-	PUSH 7 
-	PUSH 8 
-	PUSH 9 
+LEA "ASCII"
+RET 
