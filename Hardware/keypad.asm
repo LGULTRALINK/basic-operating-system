4e9f2e25822cd53ef2cf59ad659ddfd5fc406ef9
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-14 12:25:15 -0300

    Update keypad.asm

--- Hardware/keypad.asm
+++ Hardware/keypad.asm
@@ -1,11 +1,2 @@
 	CALL ["ON/OFF"]; Power-Up Display / Power-Down Display
-LEA 00H PUSH '␀'; Null State
-LEA 01H PUSH '␁'; Start of Header
-LEA 02H PUSH '␂'; Start of Text
-LEA 03H PUSH '␃'; End of Text
-LEA 04H PUSH '␄'; End of Transmission
-LEA 05H PUSH '␅'; Enquiry
-LEA 06H PUSH '␆'; Acknowledge
-LEA 07H PUSH '␇'; Bell
-LEA 08H PUSH '␈'; BackSpace
-LEA 09H PUSH '␉'; Horizontal Tabulation
+MOVSB 0000 0000, MOVSW '␀';
