1c362da7275b95669e7c442a11eda9e472fb3dd0
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-13 22:30:10 -0300

    Update keypad.asm

--- Hardware/keypad.asm
+++ Hardware/keypad.asm
@@ -1,33 +1,25 @@
-        INT	[00H],	POP	'␀',	PUSH	'�';
-	INT	[01H],	POP	'␁',	PUSH	'▲';
-	INT	[02H],	POP	'␂',	PUSH	'►';
-	INT	[03H],	POP	'␃',	PUSH	'◄';
-	INT	[04H],	POP	'␄',	PUSH	'¶';
-	INT	[05H],	POP	'␅',	PUSH	'@';
-	INT	[06H],	POP	'␆',	PUSH	'⌐';
-	INT	[07H],	POP	'␇',	PUSH	'\a';
-	INT	[08H],	POP	'␈',	PUSH	'\b';
-	INT	[09H],	POP	'␉',	PUSH	'\t';
-	INT	[10H],	POP	'␊',	PUSH	'\n';
-	INT	[11H],	POP	'␋',	PUSH	'\v';
-	INT	[12H],	POP	'␌',	PUSH	'\f';
-	INT	[13H],	POP	'␍',	PUSH	'\r';
-	INT	[14H],	POP	'␎',	PUSH	'∆';
-	INT	[15H],	POP	'␏',	PUSH	'∇';
-	INT	[16H],	POP	'␐',	PUSH	'▬';
-	INT	[17H],	POP	'␑',	PUSH	'░';
-	INT	[18H],	POP	'␒',	PUSH	'▒';
-	INT	[19H],	POP	'␓',	PUSH	'▓';
-	INT	[20H],	POP	'␔',	PUSH	'█';
-	INT	[21H],	POP	'␕',	PUSH	'¬';
-	INT	[22H],	POP	'␖',	PUSH	'';
-	INT	[23H],	POP	'␗',	PUSH	'▄';
-	INT	[24H],	POP	'␘',	PUSH	'';
-	INT	[25H],	POP	'␙',	PUSH	'';
-	INT	[26H],	POP	'␚',	PUSH	'';
-	INT	[27H],	POP	'␛',	PUSH	'';
-	INT	[28H],	POP	'␜',	PUSH	'';
-	INT	[29H],	POP	'␝',	PUSH	'';
-	INT	[30H],	POP	'␞',	PUSH	'';
-	INT	[31H],	POP	'␟',	PUSH	'';
-	INT	[32H],	POP	'␡',	PUSH	'⌂';
+	POP 'A',
+	POP 'B',
+	POP 'C',
+	POP 'D',
+	POP 'E',
+	POP 'F',
+	POP 'G',
+	POP 'H',
+	POP 'I',
+	POP 'J',
+	POP 'K',
+	POP 'L',
+	POP 'M',
+	POP 'N',
+	POP 'O',
+	POP 'Q',
+	POP 'R',
+	POP 'S',
+	POP 'T',
+	POP 'U',
+	POP 'V',
+	POP 'W',
+	POP 'X',
+	POP 'Y',
+	POP 'Z',
