a7d1ee34b8025dbe93eb0318098589d4250d014d
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-15 01:25:07 -0300

    Update sysboot.asm

--- Hardware/sysboot.asm
+++ Hardware/sysboot.asm
@@ -7,3 +7,43 @@ call FAR PTR entryPoint
 add sp, 10t ; Clean up stack
 cmp ax, SUCCESS ; Function completed successfully?
 jne error
+E820Present = FALSE;
+
+ Reg.ebx = 0;
+ do {
+ Reg.eax = 0xE820;
+ Reg.es = SEGMENT (&Descriptor);
+ Reg.di = OFFSET (&Descriptor);
+ Reg.ecx = sizeof (Descriptor);
+ Reg.edx = 'SMAP';
+ _int( 15, regs );
+ if ((Regs.eflags & EFLAG_CARRY) || Regs.eax != 'SMAP') {
+ break;
+ }
+ if (Regs.ecx < 20 || Reg.ecx > sizeof (Descriptor) ) {
+ // bug in bios - all returned descriptors must be
+ // at least 20 bytes long, and cannot be larger then
+ // the input buffer.
+ break;
+ }
+E820Present = TRUE;
+ .
+ .
+ .
+ Add address range Descriptor.BaseAddress through
+ Descriptor.BaseAddress + Descriptor.Length
+ as type Descriptor.Type
+ .
+ .
+ .
+ } while (Regs.ebx != 0);
+ if (!E820Present) {
+ .
+ .
+ .
+ call INT-15 88 and/or INT-15 E801 to obtain old style
+ memory information
+ .
+ .
+ .
+ }
