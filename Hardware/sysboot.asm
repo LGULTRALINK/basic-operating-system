2425cb9eb13607b6ce8c31cb0713bb32d3e21262
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-15 02:24:46 -0300

    Update sysboot.asm

--- Hardware/sysboot.asm
+++ Hardware/sysboot.asm
@@ -1,13 +1,13 @@
 do (eflags) {
 wrmsr [eax], int [00H]
-movsw [ecx], int [01H]
-movsb [ebx], int [02H]
-lodsw [esp], int [03H]
-lodsb [ebp], int [04H]
+rdmsr [ecx], int [01H]
+rdmsr [ebx], int [02H]
+rdmsr [esp], int [03H]
+rdmsr [ebp], int [04H]
 rdmsr [edx], int [05H]
-leahf [eip], int [06H]
-movs [eah], int [06H]
-lods [ebh], int [07H]
+wrmsr [eip], int [06H]
+rdmsr [eah], int [06H]
+rdmsr [ebh], int [07H]
 call "On switch"...
 test "Power-Up Display successfully"
 call "Off switch"...
