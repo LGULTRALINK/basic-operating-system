a8c8e9906a02e00e96fdb7e5c05c2d38b0e3a2a3
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-15 01:35:34 -0300

    Update sysboot.asm

--- Hardware/sysboot.asm
+++ Hardware/sysboot.asm
@@ -6,3 +6,14 @@
 6. CPU IP
 7. mov ecx, 08BH ;Model Specific Register to Read
 8. RDMSR ;Read Model Specific Register
+mov ecx,79h ; 79H in ECX
+
+xor eax,eax
+xor ebx,ebx
+mov ax,cs ; Segment of BIOS Update
+shl eax,4
+mov bx,offset Update ; Offset of BIOS Update
+add eax,ebx ; Linear Address of Update in EAX
+add eax,eflags ; Offset of the Update Data within the Update
+xor edx,edx ; Zero in EDX
+WRMSR ; BIOS Update Trigger
