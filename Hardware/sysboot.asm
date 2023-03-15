3e5d3c0fbb907b99ed502002742430bf4366240d
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-15 02:19:17 -0300

    Update sysboot.asm

--- Hardware/sysboot.asm
+++ Hardware/sysboot.asm
@@ -5,11 +5,13 @@ movsb [ebx], int [02H]
 lodsw [esp], int [03H]
 lodsb [ebp], int [04H]
 rdmsr [edx], int [05H]
-call "On switch"....
-iret "Standby Display"..
-call "Off switch"...
-iret "Suspend Display"..
+cmps [eip], int [06H]
 movs [eah], int [06H]
 lods [ebh], int [07H]
+call "On switch"...
+test "Power-Up Display successfully"
+call "Off switch"...
+test "Power-Down Display successfully"
+
 }
 
