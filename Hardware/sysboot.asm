ed9c9083f4c4d4d4d5a569b5d2b8b84a74ad58c7
LGULTRALINK <127228482+LGULTRALINK@users.noreply.github.com>
2023-03-15 01:36:43 -0300

    Update sysboot.asm

--- Hardware/sysboot.asm
+++ Hardware/sysboot.asm
@@ -1,6 +1,6 @@
-1. mov ecx, 08Bh ;Model Specific Register to Read
-2. xor eax,eax
-3. xor edx,edx
+1. movsw ecx,  ;Model Specific Register to Read
+2. movsb eax,eax
+3. lodsw edx,edx
 4. WRMSR ;Load 0 to MSR at 8Bh
 5. mov eax,1
 6. CPU IP
